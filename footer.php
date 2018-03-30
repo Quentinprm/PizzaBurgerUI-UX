4dd178b0b584e7c9976cdae07f429a695efe9c2a
lozatal <v.ragonneau@laposte.net>
2018-03-30 10:55:02 +0200

    fleche et centrage

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div id="footer">
 <ul>
   <!-- <li id="retour"><a href="index.php?ad=type.html">Retour</a></li> -->
-  <li id="suivant"><a href="index.php?ad=<?php echo $ad; ?>">Suivant</a></li>
+  <li id="suivant"><a href="index.php?ad=<?php echo $ad; ?>"><p>Suivant</p></a></li>
 </ul>
 
 </div>
